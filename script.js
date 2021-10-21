b2f024209bb3237768ebc75a0e72bacd25a0ffe6
aliniko <aliverchan@gmail.com>
2021-10-21 22:45:29 +0200

    Fetching the Data

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ let canvas = d3.select('#canvas')
 let drawTreeMap = () => {
 
 }
+
+
+// Fetching the Data
+// - Call the d3 json() method, giving the url of the json data
+// - Specify a function to run in .then() when the promise is resolved, that takes in the data
+//  (d3 has converted from json to javascript object) and any errors
+// - If there is an error, log it, otherwise set the movieData variable to the data
+// - Finally, run the drawTreeMap() method to use the data to draw the treemap
+
+d3.json(movieDataUrl).then(
+    (data, error) => {
+        if(error){
+            console.log(error)
+        } else {
+            movieData = data
+            console.log(movieData)
+            drawTreeMap()
+        }
+    }
+)
